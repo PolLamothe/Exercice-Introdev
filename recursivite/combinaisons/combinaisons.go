f9dfa753ae5224304c340664dd6058cb29ddf373
Pol Lamothe <lamothepol@gmail.com>
2024-01-25 15:37:39 +0100

    Résolution de combinaisons

--- recursivite/combinaisons/combinaisons.go
+++ recursivite/combinaisons/combinaisons.go
@@ -1,5 +1,7 @@
 package combinaison
 
+import "fmt"
+
 /*
 Étant donné un entier n, on souhaite trouver toutes les combinaisons d'entiers (c'est-à-dire des ensemble avec répétitions) compris entre 1 et n (inclus) dont la somme vaut n. Ainsi, par exemple, pour n = 3 on doit trouver les combinaisons {1, 1, 1}, {1, 2} et {3}.
 
@@ -21,6 +23,121 @@ Le plus simple est d'écrire une fonction récursive, mais les boucles for sont 
 2022-2023, test 2, exercice 8
 */
 
+func isInArray(index int, array []int) bool {
+	for i := 0; i < len(array); i++ {
+		if array[i] == index {
+			return true
+		}
+	}
+	return false
+}
+
+func pop(array [][]int, index []int) [][]int {
+	var result [][]int
+	for i := 0; i < len(array); i++ {
+		if !isInArray(i, index) {
+			result = append(result, array[i])
+		}
+	}
+	return result
+}
+
+func getSum(array []int) int {
+	var sum int = 0
+	for i := 0; i < len(array); i++ {
+		sum += array[i]
+	}
+	return sum
+}
+
+func GetBiggest(array []int) int {
+	var max int = 0
+	for i := 0; i < len(array); i++ {
+		if array[i] > max {
+			max = array[i]
+		}
+	}
+	return max
+}
+
+func sameContent(array1, array2 []int) bool {
+	if len(array1) != len(array2) {
+		return false
+	}
+	if GetBiggest(array1) != GetBiggest(array2) {
+		return false
+	}
+	var result []int = make([]int, GetBiggest(array1))
+	for i := 0; i < len(array1); i++ {
+		result[array1[i]-1]++
+	}
+	for i := 0; i < len(array2); i++ {
+		result[array2[i]-1]--
+	}
+	for i := 0; i < len(result); i++ {
+		if result[i] != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func recur(previous []int, length, n int) [][]int {
+	var copy []int = append([]int{}, previous...)
+	for i := len(copy) - 1; i >= 0; i-- {
+		if copy[i] < n {
+			copy[i]++
+			break
+		} else {
+			copy[i] = 1
+		}
+	}
+	var state bool = true
+	for i := 0; i < len(copy); i++ {
+		if copy[i] != n {
+			state = false
+		}
+	}
+	if state {
+		return [][]int{copy}
+	}
+	return append(recur(copy, length, n), copy)
+}
+
 func combinaisons(n int) (res [][]int) {
-	return res
+	var allCombinaisons [][]int
+	for i := 1; i <= n; i++ {
+		var previous []int = []int{}
+		for x := 0; x < i; x++ {
+			previous = append(previous, 1)
+		}
+		previous[len(previous)-1] = 0
+		var copy []int = append([]int{}, previous...)
+		var temp [][]int = recur(copy, i, n)
+		allCombinaisons = append(allCombinaisons, temp...)
+	}
+	var banIndex []int = []int{}
+	for i := 0; i < len(allCombinaisons); i++ {
+		if getSum(allCombinaisons[i]) != n {
+			banIndex = append(banIndex, i)
+		}
+	}
+	allCombinaisons = pop(allCombinaisons, banIndex)
+	banIndex = []int{}
+	for j := 0; j < len(allCombinaisons); j++ {
+		for x := 0; x < len(allCombinaisons); x++ {
+			if j != x {
+				if sameContent(allCombinaisons[j], allCombinaisons[x]) {
+					if j < x {
+						banIndex = append(banIndex, x)
+					} else {
+						banIndex = append(banIndex, j)
+					}
+				}
+			}
+		}
+	}
+	allCombinaisons = pop(allCombinaisons, banIndex)
+	fmt.Println(allCombinaisons)
+	return allCombinaisons
 }
