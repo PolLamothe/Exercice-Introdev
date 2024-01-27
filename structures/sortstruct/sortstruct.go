54fdcffb9a0acba850092c476c1e27f8cd13cb04
Pol Lamothe <lamothepol@gmail.com>
2024-01-27 21:56:23 +0100

    Résolution de sortstruct

---
+++ structures/sortstruct/sortstruct.go
@@ -0,0 +1,32 @@
+package sortstruct
+
+/*
+On souhaite classer un groupe d'étudiants en fonction des notes obtenues à un contrôle. On représente les étudiants par la structure student et on les place dans un tableau. L'objectif de la fonction sortByMark est de trier ce tableau de manière à ce que les étudiants soient classés de celle ou celui qui a eu la meilleure note à celle ou celui qui à eu la moins bonne note.
+
+On pourra considérer qu'il n'y a jamais deux étudiants qui ont la même note.
+
+# Entrée
+- t : un tableau d'étudiants qui doit être trié en place de la meilleure note à la moins bonne note
+
+# Info
+2023-2024, test 3, exercice 3
+*/
+
+type student struct {
+	firstName string
+	lastName  string
+	age       int
+	mark      float64
+}
+
+func sortByMark(t []student) {
+	for i := 0; i < len(t); i++ {
+		for j := i + 1; j < len(t); j++ {
+			if t[i].mark < t[j].mark {
+				t[i], t[j] = t[j], t[i]
+				sortByMark(t)
+				return
+			}
+		}
+	}
+}
